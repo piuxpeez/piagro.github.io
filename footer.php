083e7a5778d0863a130b12e4d8c91ae9a98b8e97
piuxpeez <okonjielozonampius@gmail.com>
2021-04-24 13:06:12 -1100

    UPDATED

---
+++ footer.php
@@ -0,0 +1,15 @@
+<style>
+
+    b{color:red;}
+    h3{background:black;
+    color:gray;
+    bottom:0;
+    position:;}
+</style>
+
+
+<?php
+
+echo "<h3>theme designed by <c>Piux Inc.</c></h3>."
+
+?>
